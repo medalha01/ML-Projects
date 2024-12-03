c03f937adea816ec4e97a93189812809bba74b58
medalha01 <hiperboss2009@gmail.com>
2024-12-03 00:01:38 -0300

    Starting inits

---
+++ src/neural_network/__init__.py
@@ -0,0 +1,30 @@
+from .activations import (
+    get_activation,
+    relu,
+    relu_derivative,
+    sigmoid,
+    sigmoid_derivative,
+    tanh,
+    tanh_derivative,
+    leaky_relu,
+    leaky_relu_derivative,
+    softmax,
+    softmax_derivative,
+    linear,
+    linear_derivative,
+    elu,
+    elu_derivative,
+)
+from .losses import MeanSquaredError, CrossEntropyLoss, HingeLoss
+from .optimizer import GradientDescent
+from .utils import (
+    initialize_weights,
+    orthogonal_init,
+    normalize_data,
+)
+from .layers import (
+    DenseLayer,
+    L2DenseLayer,
+    DropoutLayer,
+    Layer,
+)
---
+++ src/neural_network/layers/__init__.py
@@ -0,0 +1,4 @@
+from .abstract_layer import Layer
+from .dense_layer import DenseLayer
+from .dense_l2_layer import L2DenseLayer
+from .dropout_layer import DropoutLayer
